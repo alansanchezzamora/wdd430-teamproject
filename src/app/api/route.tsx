574b4111e344fa444502c436c0b44e86138ca322
alansanchezzamora <122119972+alansanchezzamora@users.noreply.github.com>
2024-12-18 18:27:36 -0600

    fix?

---
+++ src/app/api/route.tsx
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { db } from "../../lib/db";
+
+export async function GET() {
+  const res = await db`SELECT * FROM products`; // Consulta de todos los productos
+  return NextResponse.json(res.rows);
+}
+
+export async function POST(request: Request) {
+  const { title, description, price, imageUrl, category, sellerId } =
+    await request.json();
+  const res = await db`
+    INSERT INTO products (title, description, price, image_url, category, seller_id)
+    VALUES (${title}, ${description}, ${price}, ${imageUrl}, ${category}, ${sellerId})
+    RETURNING *`;
+  return NextResponse.json(res.rows[0]);
+}
